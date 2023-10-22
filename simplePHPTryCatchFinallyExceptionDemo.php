fac2420693eea0a78b8b715ad627c6b34110ad9d
spenright <stephen.p.enright@gmail.com>
2023-10-21 17:28:06 -0700

    PHP Function, Try/Catch/Finally, Exception. Demonstration of: 1. A function with one parameter. 2. A Try/Catch/Finally block. 3. Use of Exception and display of its message.

---
+++ simplePHPTryCatchFinallyExceptionDemo.php
@@ -0,0 +1,42 @@
+<?php
+
+    //
+    // Not my code.
+    //
+    // Sample of:
+    // 1. a funtion
+    // 2. try/catch/finally
+    // 3. Exception
+    //
+    // No warrenty.
+    // No claims.
+    //
+    function onedividedby($x) {
+        if (!$x) {
+            throw new Exception( "Division by zero.\n" );
+        }
+        return 1/$x;
+    }
+
+    // The 'Main' execution starts here
+    //
+    try {
+        echo onedividedby(5) . "\n";
+    } catch (Exception $e) {
+        echo "Exception trapped: ",  $e->getMessage(), "\n";
+    } finally {
+        echo "First finally.\n";
+    }
+
+    try {
+        echo onedividedby(0) . "\n";
+    } catch (Exception $e) {
+        echo "Caught exception: ",  $e->getMessage(), "\n";
+    } finally {
+        echo "Second finally.\n";
+    }
+    //
+    // Finished execution.
+    echo "Done.\n";
+
+?>
